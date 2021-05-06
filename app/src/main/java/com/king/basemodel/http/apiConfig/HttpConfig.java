177756a9edf45ee1ec53f4935d4dc96b76ea348b
18780106683 <18780106683@163.com>
2021-05-06 09:19:46 +0800

    no message

---
+++ app/src/main/java/com/king/basemodel/http/apiConfig/HttpConfig.java
@@ -0,0 +1,12 @@
+package com.king.basemodel.http.apiConfig;
+
+/**
+ * @ClassName HttpConfig
+ * @Description TODO
+ * @Author ：WangZhuang
+ * @Date : 2020/10/15 17:11
+ * @Version :
+ */
+public class HttpConfig {
+    public final static boolean IS_UAT = false;
+}
